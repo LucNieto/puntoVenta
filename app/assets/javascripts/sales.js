c7d5d7ce0d32f32ebd7f2369c4ccb00ea6743ab8
taniatorresa <tania.torresa>
2018-05-17 16:23:52 +0000

    nested forms implementados con cocoon 2

--- app/assets/javascripts/sales.js
+++ app/assets/javascripts/sales.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  console.log("a ver");
 
-})
+    $('#details').on('cocoon:before-insert', function() {
+    
+      console.log(" DARIO BBB");
+     
+    })
+  
+    
+  });
