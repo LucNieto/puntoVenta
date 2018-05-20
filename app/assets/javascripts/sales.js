0db745847bf8503c939ee19e9fa776207bf3f74a
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-05-19 23:21:52 -0500

    [#157639718] Agregando funcionalidad al codigo de javascript 	deleted:    PUNTOVENTA.code-workspace 	deleted:    app/assets/javascripts/bussines_names.coffee 	deleted:    app/assets/javascripts/companies.coffee 	deleted:    app/assets/javascripts/products.coffee 	deleted:    app/assets/javascripts/reports/products.coffee 	deleted:    app/assets/javascripts/reports/sales.coffee 	deleted:    app/assets/javascripts/sales.coffee 	modified:   app/assets/javascripts/sales.js 	deleted:    app/assets/javascripts/welcome.coffee 	modified:   app/views/sales/_detail_fields.html.erb 	modified:   app/views/sales/_form.html.erb

--- app/assets/javascripts/sales.js
+++ app/assets/javascripts/sales.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
-  console.log("a ver");
-
-    $('#details').on('cocoon:before-insert', function() {
-    
-      console.log(" DARIO BBB");
-     
-    })
-  
-    
+  $(document).on('change keyup', '.input_cantidad, .input_importe', function(){
+    method.recalculate();
   });
+
+  method = {
+    recalculate: function(){
+      neto = 0;
+      var $row = $(".nested-fields").children();
+      // obtenemos todas las filas(cada que se presiona Agregar detalle se agrega un row con N elementos)
+      var cantidad = 0, importe = 0, saldo = 0;
+      $.each($row.children(), function(index, group){
+        $elements = $( group ).children().children();
+        // Obtenemos información de cada elemento
+        $.each( $elements, function(index, element){
+          $this = $( element );
+          if( $this.hasClass('input_cantidad') ){
+            cantidad = parseFloat( $this.val() );
+          }else if( $this.hasClass('input_importe') ){
+            importe = parseFloat( $this.val() );
+          }else if( $this.hasClass('col-form-label') ){
+            saldo = (cantidad * importe ).toFixed(2);
+            $this.html( saldo );
+            neto += parseFloat(saldo);
+          }
+        }); 
+      });
+      
+      $("#sale_neto").val( neto.toFixed(2) );
+      $("#sale_neto_iva").val( (neto * 0.16).toFixed(2) );
+      $("#sale_total_compra").val( (neto * 1.16).toFixed(2) );
+    }
+  }
+  
+});
