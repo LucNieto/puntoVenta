42e51050a87225466cc83b02aeaef13ca72250f7
taniatorresa <tania.torresa>
2018-05-23 21:18:52 +0000

    Neto x

--- app/assets/javascripts/sales.js
+++ app/assets/javascripts/sales.js
@@ -1,19 +1,6 @@
  $(document).ready(function() {
-console.log('ejel');
-  $('#details')
-  .on('cocoon:before-insert', function(e,task_to_be_added) {
-    task_to_be_added.fadeIn('slow');
-  })
-  .on('cocoon:after-insert', function(e, added_task) {
-    // e.g. set the background of inserted task
-    added_task.css("background","red");
-  })
-  .on('cocoon:before-remove', function(e, task) {
-    // allow some time for the animation to complete
-    $(this).data('remove-timeout', 1000);
-    task.fadeOut('slow');
-  });
-  
+
+
 
   $(document).on('change keyup', '.input_cantidad, .input_importe', function(){
     method.recalculate();
